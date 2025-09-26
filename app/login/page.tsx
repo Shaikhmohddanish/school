6519030f3d73ebf746f03a9bf8f9725ccd3b31b3
Shaikhmohddanish <danishshaikh2365@gmail.com>
2025-09-26 21:14:24 +0530

    updated

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -84,7 +84,7 @@ export default function Login() {
                 autoComplete="email"
                 required
                 className="relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                placeholder="Email address"
+                placeholder="Email your address"
                 value={email}
                 onChange={(e) => {
                     setEmail(e.target.value);
